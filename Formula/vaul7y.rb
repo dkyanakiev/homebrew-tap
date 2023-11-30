92b0b0b767ca0032d48d6a7675b9037f554d8f71
Dimitar Yanakiev <dkyanakiev@gmail.com>
2023-11-30 23:57:31 +0200

    Brew formula update for vaul7y version v0.0.4

--- Formula/vaul7y.rb
+++ Formula/vaul7y.rb
@@ -5,11 +5,11 @@
 class Vaul7y < Formula
   desc "A simple terminal application/TUI for interacting with HashiCorp Vault."
   homepage "https://github.com/dkyanakiev/vaul7y"
-  version "0.0.3"
+  version "0.0.4"
 
   on_macos do
-    url "https://github.com/dkyanakiev/vaul7y/releases/download/v0.0.3/vaul7y_0.0.3_Darwin_all.tar.gz"
-    sha256 "eb9b6cc714b20465ff27113de167eed59ab4d87f69f4b103f456c51b56a478ea"
+    url "https://github.com/dkyanakiev/vaul7y/releases/download/v0.0.4/vaul7y_0.0.4_Darwin_all.tar.gz"
+    sha256 "3cc6dffbd4a6888aaafcef1a101b23b5c0189261796ab9b63abd5e471af94f94"
 
     def install
       bin.install "vaul7y"
@@ -18,16 +18,16 @@ class Vaul7y < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dkyanakiev/vaul7y/releases/download/v0.0.3/vaul7y_0.0.3_Linux_arm64.tar.gz"
-      sha256 "6f89f279764aa0822b4b7a8fb6b20b5f98a22211cd5294aceca78015d995d01d"
+      url "https://github.com/dkyanakiev/vaul7y/releases/download/v0.0.4/vaul7y_0.0.4_Linux_arm64.tar.gz"
+      sha256 "50d1537bd7e2e8995f68cb4ed952ccf2ce9ad928df9250c796e60f67d5bae84f"
 
       def install
         bin.install "vaul7y"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/dkyanakiev/vaul7y/releases/download/v0.0.3/vaul7y_0.0.3_Linux_x86_64.tar.gz"
-      sha256 "66ecdfbb4997e766199676284d636efb736586dd21dce21384def054a20a13c9"
+      url "https://github.com/dkyanakiev/vaul7y/releases/download/v0.0.4/vaul7y_0.0.4_Linux_x86_64.tar.gz"
+      sha256 "6851014d2a3c0b80b9dedef3fc8e8c2b9a7a50548ccc6ffa83d6d86b9d2bc98e"
 
       def install
         bin.install "vaul7y"
